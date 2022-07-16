c372112934e5b8b9a976a60e675e2a2dd866023c
tomotakashimizu <tomotaka.developer@gmail.com>
2022-07-16 21:02:38 +0900

    clean: remove func

--- card.go
+++ card.go
@@ -1,5 +1,7 @@
 package gacha
 
+import "fmt"
+
 type Rarity string
 
 const (
@@ -9,15 +11,11 @@ const (
 	RarityXR Rarity = "XR"
 )
 
-func (r Rarity) String() string {
-	return string(r)
-}
-
 type Card struct {
 	Rarity Rarity // レア度
 	Name   string // 名前
 }
 
 func (c *Card) String() string {
-	return c.Rarity.String() + ":" + c.Name
+	return fmt.Sprintf("%v:%v", c.Rarity, c.Name)
 }
